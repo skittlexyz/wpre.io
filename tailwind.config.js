222bff49a8fabf077ceb929d5e27f2f984f5b1e1
Desenvolvimento <dsadm@educ501.local>
2024-08-21 16:51:34 -0300

    Doing the height responsive styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ export default {
         'black': '#151515'
       }
     },
+    screens: {
+      'default': { 'raw': '(min-height: 400px)' },
+
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
